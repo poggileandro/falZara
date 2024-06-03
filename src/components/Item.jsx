4d154698bf20ba7677f43932a0c475a81fdc85ab
LeandroPoggi <poggileandro@gmail.com>
2024-06-03 20:54:12 -0300

    tercer commit

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Item = ({producto}) => {
+
+
+export const Item = ({ producto }) => {
+
+
     return (
         <div className='producto'>
-            <img src={producto.imagen} alt="" />
+            <img className='producto-imagen' src={producto.imagen} alt={producto.nombre} />
             <h2>{producto.nombre}</h2>
             <p> ${producto.precio}</p>
             <Link to={`/item/${producto.id}`}>Ver Detalle</Link>        
-            </div>
-      )
+        </div>
+    )
 }
